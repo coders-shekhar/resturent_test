868def67f6eb111de40a67be087f15357ba3f6e9
Shashwat <codersshekhar@gmail.com>
2025-02-18 22:12:45 +0530

    3rd commit

--- db.js
+++ db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 require("dotenv").config();
 
 // const mongoURI = "mongodb://127.0.0.1:27017/resturent";
-const mongoURI = process.env.mongoURI;
+const mongoURI = process.env.mongoURL;
 
 mongoose.connect(mongoURI);
 
